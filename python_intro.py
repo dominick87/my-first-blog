7ac4f805baf3cd294a520fec6e044c642ca9065e
Dominick <domenicotrovato87@gmail.com>
2017-03-26 14:27:05 -0700

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,41 @@
+
+
+print('Hello, Django Girls')
+
+
+if 3>2:
+	print('it is greater')
+else: 
+	print("it is not greater")
+
+name = 'Nick'
+
+if name == 'Bob':
+	print('hi Bob')
+elif name == 'Nick':
+	print('hi Nick')
+	print('hello again')
+	x=5
+	print(x)
+else:
+	print('hi stranger!')
+
+print(x)
+
+
+#this is a comment
+
+def hi():
+	print('hi there!')
+	print('how are you?')
+
+hi()
+
+print()
+print('the sum of the number is:')
+
+def sum_the_numbers(a,b,c):
+	print(a+b+c)
+
+sum_the_numbers(3,4,5)
+
